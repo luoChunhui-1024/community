ea9b1fececab5b9cdb456ede3252df30555e765f
LuoChunhui <3254014978@qq.com>
2020-11-01 10:35:42 +0800

    认识事务，使用事务的编程式事务和声明式事务实现事务

--- src/main/java/com/nowcoder/work/community/service/AlphaService.java
+++ src/main/java/com/nowcoder/work/community/service/AlphaService.java
@@ -1,12 +1,25 @@
 package com.nowcoder.work.community.service;
 
 import com.nowcoder.work.community.dao.AlphaDao;
+import com.nowcoder.work.community.dao.DiscussPostMapper;
+import com.nowcoder.work.community.dao.UserMapper;
+import com.nowcoder.work.community.entity.DiscussPost;
+import com.nowcoder.work.community.entity.User;
+import com.nowcoder.work.community.util.CommunityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.util.Date;
 
 @Service
 // @Scope("prototype")
@@ -14,6 +27,15 @@ public class AlphaService {
 
     @Autowired
     private AlphaDao alphaDao;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    @Autowired
+    private TransactionTemplate transactionTemplate;
 
     public AlphaService(){
         System.out.println("实例化AlphaService");
@@ -31,5 +53,62 @@ public class AlphaService {
 
     public String find(){
         return alphaDao.select();
+    }
+
+    @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+    public Object save1(){
+        // 新增用户
+        User user = new User();
+        user.setUsername("alpha");
+        user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
+        user.setPassword(CommunityUtil.md5("123" + user.getSalt()));
+        user.setEmail("alpha@qq.com");
+        user.setHeaderUrl("http://image.nowcoder.com/head/99t.png");
+        user.setCreateTime(new Date());
+        userMapper.insertUer(user);
+
+        // 新增帖子
+        DiscussPost post = new DiscussPost();
+        post.setUserId(user.getId());
+        post.setTitle("hello");
+        post.setContent("新人报道！");
+        post.setCreateTime(new Date());
+        discussPostMapper.insertDiscussPost(post);
+
+        Integer.valueOf("abc");
+        return "ok";
+    }
+
+    // 编程式事务
+    public Object save2(){
+
+        transactionTemplate.setIsolationLevel(TransactionDefinition.ISOLATION_READ_COMMITTED);
+        transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+
+        return transactionTemplate.execute(new TransactionCallback<Object>() {
+            @Override
+            public Object doInTransaction(TransactionStatus transactionStatus) {
+                // 新增用户
+                User user = new User();
+                user.setUsername("alpha");
+                user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
+                user.setPassword(CommunityUtil.md5("123" + user.getSalt()));
+                user.setEmail("alpha@qq.com");
+                user.setHeaderUrl("http://image.nowcoder.com/head/99t.png");
+                user.setCreateTime(new Date());
+                userMapper.insertUer(user);
+
+                // 新增帖子
+                DiscussPost post = new DiscussPost();
+                post.setUserId(user.getId());
+                post.setTitle("hello");
+                post.setContent("新人报道！");
+                post.setCreateTime(new Date());
+                discussPostMapper.insertDiscussPost(post);
+
+                // Integer.valueOf("abc");
+                return "ok";
+            }
+        });
     }
 }
---
+++ src/test/java/com/nowcoder/work/community/TransactionTest.java
@@ -0,0 +1,29 @@
+package com.nowcoder.work.community;
+
+import com.nowcoder.work.community.service.AlphaService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class TransactionTest {
+
+    @Autowired
+    private AlphaService alphaService;
+
+    @Test
+    public void testSave1(){
+        Object obj = alphaService.save1();
+        System.out.println(obj);
+    }
+    @Test
+    public void testSave2(){
+        Object obj = alphaService.save2();
+        System.out.println(obj);
+    }
+}
